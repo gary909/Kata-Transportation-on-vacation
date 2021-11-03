a7b87f5c8aeeb94f609ef071de2150342939db7f
gary <gary.white@lodgit.com>
2021-11-03 13:08:38 +0100

    template code

---
+++ kataTransport.js
@@ -0,0 +1,10 @@
+function rentalCarCost(d) {
+    // Your solution here
+}
+
+console.log(rentalCarCost(1)); // Returns 40
+console.log(rentalCarCost(2)); // Returns 80
+console.log(rentalCarCost(3)); // Returns 100
+console.log(rentalCarCost(4)); // Returns 140
+console.log(rentalCarCost(5)); // Returns 180
+console.log(rentalCarCost(6)); // Returns 220
