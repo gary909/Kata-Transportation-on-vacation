9c1a5fc301f1c8ca0f0d53756aac8c92add8dd39
gary <gary.white@lodgit.com>
2021-11-03 13:10:11 +0100

    more tests added

--- kataTransport.js
+++ kataTransport.js
@@ -2,9 +2,16 @@ function rentalCarCost(d) {
     // Your solution here
 }
 
+
 console.log(rentalCarCost(1)); // Returns 40
 console.log(rentalCarCost(2)); // Returns 80
+
 console.log(rentalCarCost(3)); // Returns 100
 console.log(rentalCarCost(4)); // Returns 140
 console.log(rentalCarCost(5)); // Returns 180
 console.log(rentalCarCost(6)); // Returns 220
+
+console.log(rentalCarCost(7)); // Returns 230
+console.log(rentalCarCost(8)); // Returns 270
+console.log(rentalCarCost(9)); // Returns 310
+console.log(rentalCarCost(10)); // Returns 350
