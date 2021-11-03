2e684ad0b02cfb78efcfd2c64b1ea66fdde6aa39
gary <gary.white@lodgit.com>
2021-11-03 13:16:38 +0100

    code working

--- kataTransport.js
+++ kataTransport.js
@@ -1,5 +1,11 @@
 function rentalCarCost(d) {
-    // Your solution here
+    if (d >= 7) {
+        return (d * 40) - 50;
+    } else if (d < 7 && d >= 3) {
+        return (d * 40) - 20;
+    } else {
+        return d * 40;
+    }
 }
 
 
